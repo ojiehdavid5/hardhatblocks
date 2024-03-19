46a3165c2fceccfe33d1afb7e676eb4cab7136e6
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-03-19 13:17:03 +0100

    changes

---
+++ script.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const web3=require('web3');
+
+var contract;
+
+$(document).ready(function(){
+    web3= new Web3(web3.currentProvider);
+
+
+var address=process.env.ADDRESS;
+console.log(address);
+    contract=new web3.eth.Contract(abi, address);
+
+    contract.methods.getBalance().call().then(function(bal){
+        $('balance').html(bal);
+    })
+});
